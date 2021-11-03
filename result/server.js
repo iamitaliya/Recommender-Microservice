202cd3de2d4c131f6c5779e83b84871fc0b87d77
Nikhilkumar Italiya <nikhilkumar.italiya@st.ovgu.de>
2021-11-03 12:24:39 +0100

    added integration test

--- result/server.js
+++ result/server.js
@@ -30,46 +30,125 @@ function openPort(app) {
     app.listen(2411);
     }
     
-    const { Client } = require('pg');
-    const { json } = require('express');
-    const client = new Client({
-        user: "admin",
-        password: "admin",
-        host: "movie_database",
-        port: 5432,
-        database: "movie_rec"
-    })
+const { Pool } = require('pg');
+const { json } = require('express');
+const client = new Pool({
+    user: "admin",
+    password: "admin",
+    host: "movie_database",
+    port: 5432,
+    database: "movie_rec"
+})
     
 
-loadData().then(() => {
-    execute(app)}).catch(err => {
-        console.log(err)
-    })
+loadData()
 
 async function loadData() {
     try {
-        const result = await fetch("http://collector:2111/api/load-data/MOVIES").then(response => response.json()).then(data => {
-            console.log(data)
-            if (data.status === "success") {
-                console.log("data loaded")
-            } else {
-                console.log("data not loaded")
+        // ------------------Integration testing--------------------------
+        // check status of required microservices
+        console.log("---------------------------- INTEGRATION TESTING STARTED --------------------------")
+        const services = { collector : "http://collector:2111/api/check-status",
+                           recommender : "http://recommender:2211/api/check-status",
+                           dashboard : "http://dashboard:5555/api/check-status",
+                           database : "visualiser database"
+                            }
+        const res_services = {}
+        for(const [service, url] of Object.entries(services))
+        {
+            if(service == "database")
+            {
+                res_services[service] = await check_db("select 2 + 2 as count")
+            }else{
+                res_services[service] = await check_status(url)
             }
-        }).catch(err => {   
-            console.log(err)
-        })
-        return result
+            console.log(service + ": " + (res_services[service] ? "PASSED"  : "FAILED") )
+        }
+        const all_passed = Object.values(res_services).every(Boolean)
+        if(!all_passed){
+            return integration_failed()
+        }
+        // check if there is data already available in database
+        console.log("Checking data in database")
+        var query = "select count(*) from movie_table"
+        const table_rows = await check_db(query)
+        
+        if(table_rows < 5){
+            console.log("No data found! \nLoading the data... \nThis will take around 5 minutes...")
+             // if there is no data then, get the data
+            const result = await fetch("http://collector:2111/api/load-data/MOVIES").then(response => response.json()).then(data => {
+                if (data.status === "success") {
+                    console.log("Data loaded successfully")
+                } else {
+                    console.log("Data loading failed")
+                }
+            }).catch(err => {   
+                // console.log(err)
+                console.log("Data loading failed")
+            })
+        }
+
+        // passing dummy data to see if we have data
+        fetch('http://recommender:2211/get-recommendation/Iron%20Man%20(2008)').then(function (response) {
+            // The API call was successful!
+            return response.text();
+        }).then(function (html) {
+            // This is the HTML from our response as a text string
+            if(html.length > 2){
+                return integration_passed()
+            }else{
+                return integration_failed()
+            }
+        }).catch(function (err) {
+            // There was an error
+            return integration_failed(err)
+        });
     }
     catch (ex) {
-        console.log(ex)
+        return integration_failed(err)
     }
 }
+
+function integration_failed(err = 0){
+    // console.log(err)
+    console.log("Error occured while testing.\n---------------------------- INTEGRATION TESTING ENDED -------------------------- \nStopping the Service.")
+}
+function integration_passed(){
+    console.log("Integration Test Passed.\n ---------------------------- INTEGRATION TESTING ENDED -------------------------- \nStarting the Service.")
+    execute(app).catch(err => {
+        console.log(err)
+    })
+}
+
+async function check_db(query){
+    return client.query(query).then(function (result) {
+        return result.rows[0]['count']
+    }).catch(err => {
+        // console.log(err) 
+        return 0
+    })
+}
+
+// to check status of microservices
+async function check_status(url)
+{
+    return fetch(url).then(response => response.json()).then(res => {
+    if(res.status == "success"){ 
+        return true
+    }else{
+        return false
+    }
+    }).catch((error) => {
+        return false
+    })
+}
+
 
 async function execute(app) {
     try {
         openPort(app)
         await client.connect()
-        console.log("Connected successfully")
+        // console.log("Connected successfully")
         movies = await client.query("select title from movie_table")
         // bar_data = toRows(movies.rows, movies.rowCount)
         var arr1 = new Array()
@@ -79,14 +158,12 @@ async function execute(app) {
         arr1 = [].concat.apply([], arr1)
         toJSON(arr1)
         // console.log(arr1)
-        await client.end()
-        console.log("Client disconnected")
+        // console.log("Client disconnected")
     }
     catch (ex) {
         console.log("Error : " + ex)
     }
     finally {
-        client.end()
         console.log("client disconnected")
     }
 }
@@ -102,8 +179,6 @@ function toJSON(data) {
         }
         return false
     })
-
-
 }
 
 module.exports = toJSON;
